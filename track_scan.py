d15a3f2889faa7f1a57a385a9382a29864372dae
wuxiaolong <wuxiaolong@p1.com>
2022-08-29 11:25:20 +0800

    MOD: 新方式匹配所有埋点

--- track_scan.py
+++ track_scan.py
@@ -16,6 +16,14 @@ tracking_method_name_map = {
     "sendBP": [],
     "createPageHelper": [],
 }
+# 此处必须加方法返回值，否则会匹配代码调用处的pageId()、this.pageId()、act().pageId()、getPageId()
+page_id_java_method_name_pageId = "String pageId()"
+# fun getPageId(): String
+# override fun pageId() = "p_matchmaker_purchase_page"
+
+page_id_kt_method_name_getPageId = "fun getPageId()"
+page_id_kt_method_name_pageId = "fun pageId()"
+page_id_kt_method_name_pageId_one_line = "fun pageId() ="
 
 valid_pid_map = {
 
@@ -50,6 +58,38 @@ def find_all_file_with_suffix(dir, suffix, fl):
             find_all_file_with_suffix(new_dir, suffix, fl)
     return fl
 
+pid_rule = '"p_'
+eid_rule = '"e_'
+def find_all_id_method(f):
+    file = open(f, "r")
+    lines = file.readlines()
+    for line in lines:
+        if line.__contains__(pid_rule):
+            index = line.find(pid_rule)
+            temp = line[index:]
+            params = temp.split('"')
+            pid = params[1]
+            if pid in invalid_pid_map:
+                valid_pid_map[pid] += 1
+            else:
+                valid_pid_map[pid] = 1
+            # # print('index->'+format(index))
+            # # print('temp->'+format(temp))
+            # # print('params->'+format(params))
+            # print(pid)
+        if line.__contains__(eid_rule):
+            index = line.find(eid_rule)
+            temp = line[index:]
+            params = temp.split('"')
+            eid = params[1]
+            # print('index->'+format(index))
+            # print('temp->'+format(temp))
+            # print('params->'+format(params))
+            # print(eid)
+            if eid in valid_eid_map:
+                valid_eid_map[eid] += 1
+            else:
+                valid_eid_map[eid] = 1
 
 # 此方法已废弃，只维护 find_all_kt_track_method
 def find_all_java_track_method(f):
@@ -109,16 +149,16 @@ def find_all_java_track_method(f):
                             if len(eidParams) > 1:
                                 # 这种才是正常情况，用双引号包裹的
                                 eid = eidParams[1]
-                                if pid in valid_eid_map:
-                                    valid_eid_map[pid] += 1
+                                if eid in valid_eid_map:
+                                    valid_eid_map[eid] += 1
                                 else:
-                                    invalid_eid_map[pid] = 1
+                                    valid_eid_map[eid] = 1
                             else:
                                 eid = eidParams[0]
-                                if pid in invalid_eid_map:
-                                    invalid_eid_map[pid] += 1
+                                if eid in invalid_eid_map:
+                                    invalid_eid_map[eid] += 1
                                 else:
-                                    invalid_eid_map[pid] = 1
+                                    invalid_eid_map[eid] = 1
                                 # print('此处eid不能直接使用，请手动处理')
 
                             print('pid->' + pid)
@@ -144,6 +184,61 @@ def find_all_java_track_method(f):
                         #     print('eid->'+eid)
 
 
+def find_pageid_method(f):
+    file = open(f, "r")
+    lines = file.readlines()
+    is_need_go_ahead = False
+    for line in lines:
+        if line.strip().endswith(tracking_class_name_with_package_kt) or line.strip().endswith(tracking_class_name_with_package_java):
+            is_need_go_ahead = True
+            break
+    if is_need_go_ahead:
+        line_feed = False
+        has_get_page_id = False
+        has_page_id_java = False
+        has_page_id_kt = False
+        pid = ''
+        for line in lines:
+            if line_feed:
+                line_feed = False
+                if has_page_id_java:
+                    index = line.find('return')
+                    temp = line[index+6:]
+                    pid_params = temp.split('"')
+                    if len(pid_params) > 2:
+                        pid = pid_params[1]
+                    if pid in invalid_pid_map:
+                        valid_pid_map[pid] += 1
+                    else:
+                        valid_pid_map[pid] = 1
+                if has_page_id_kt:
+                    pid_params = line.split('"')
+                    # print('pid_params->' + format(pid_params))
+            # print('line->' + format(line))
+
+            # print('__contains__->' + format(line.strip().__contains__(page_id_kt_method_name_pageId_one_line)))
+            if line.strip().__contains__(page_id_kt_method_name_pageId_one_line):
+                pid_params = line.split('"')
+                if len(pid_params) > 2:
+                    pid = pid_params[1]
+                print('pid->' + format(pid))
+                if pid in invalid_pid_map:
+                    valid_pid_map[pid] += 1
+                else:
+                    valid_pid_map[pid] = 1
+                break
+            if line.__contains__(page_id_java_method_name_pageId):
+                line_feed = True
+                has_page_id_java = True
+            if line.__contains__(page_id_kt_method_name_getPageId) :
+                line_feed = True
+                has_get_page_id = True
+            if line.__contains__(page_id_kt_method_name_pageId):
+                line_feed = True
+                has_page_id_kt = True
+                print('line_feed->' + format(line_feed))
+            else:
+                line_feed = False
 
 def find_all_kt_track_method(f):
     file = open(f, "r")
@@ -224,9 +319,9 @@ def find_all_kt_track_method(f):
                                 # 这种才是正常情况，用双引号包裹的
                                 eid = eidParams[1]
                                 if pid in valid_eid_map:
-                                    valid_eid_map[pid] += 1
+                                    valid_eid_map[eid] += 1
                                 else:
-                                    invalid_eid_map[pid] = 1
+                                    invalid_eid_map[eid] = 1
                             else:
                                 eid = eidParams[0]
                                 if pid in invalid_eid_map:
@@ -251,11 +346,17 @@ if __name__ == '__main__':
     java_file_list = []
 
 
-    # for f_java in find_all_file_with_suffix(project_path, ".java", java_file_list):
-    #     # print("java->" + f_java)
-    #     find_all_java_track_method(f_java)
+    for f_java in find_all_file_with_suffix(project_path, ".java", java_file_list):
+        # print("java->" + f_java)
+        find_all_id_method(f_java)
+    for f_kt in find_all_file_with_suffix(project_path, ".kt", java_file_list):
+        # print("f_kt->" + f_kt)
+        find_all_id_method(f_kt)
     # #
     # for f_kt in find_all_file_with_suffix(project_path, ".kt", kt_file_list):
+    #     # print("kt->" + f_kt)
+    #     find_all_kt_track_method(f_kt)
+    # for f_kt in find_all_file_with_suffix(project_path, ".java", java_file_list):
     #     # print("kt->" + f_kt)
     #     find_all_kt_track_method(f_kt)
 
@@ -269,21 +370,22 @@ if __name__ == '__main__':
     for (key, value) in valid_pid_map.items():
         valid_pid_key_count += 1
         valid_pid_count += valid_pid_map[key]
+        pid_file.write(key + '\n')
     for (key, value) in invalid_pid_map.items():
         invalid_pid_key_count += 1
         invalid_pid_count += invalid_pid_map[key]
 
-    # print('valid_pid_key_count->' + format(valid_pid_key_count))
-    # print('valid_pid_count->' + format(valid_pid_count))
-    #
+    print('valid_pid_key_count->' + format(valid_pid_key_count))
+    print('valid_pid_count->' + format(valid_pid_count))
+
     # print('invalid_pid_key_count->' + format(invalid_pid_key_count))
     # print('invalid_pid_count->' + format(invalid_pid_count))
 
     pid_key_count = valid_pid_key_count + invalid_pid_key_count
     pid_count = valid_pid_count + invalid_pid_count
 
-    print('pid_key_count->' + format(pid_key_count))
-    print('pid_count->' + format(pid_count))
+    # print('pid_key_count->' + format(pid_key_count))
+    # print('pid_count->' + format(pid_count))
 
     valid_eid_key_count = 0
     valid_eid_count = 0
@@ -295,26 +397,28 @@ if __name__ == '__main__':
     for (key, value) in valid_eid_map.items():
         valid_eid_key_count += 1
         valid_eid_count += valid_eid_map[key]
+        eid_file.write(key + '\n')
     for (key, value) in invalid_eid_map.items():
         invalid_eid_key_count += 1
         invalid_eid_count += invalid_eid_map[key]
 
-    # print('valid_eid_key_count->' + format(valid_eid_key_count))
-    # print('valid_eid_count->' + format(valid_eid_count))
-    #
+    print('valid_eid_key_count->' + format(valid_eid_key_count))
+    print('valid_eid_count->' + format(valid_eid_count))
+
     # print('invalid_eid_key_count->' + format(invalid_eid_key_count))
     # print('invalid_eid_count->' + format(invalid_eid_count))
 
     eid_key_count = valid_eid_key_count + invalid_eid_key_count
     eid_count = valid_eid_count + invalid_eid_count
 
-    print('eid_key_count->' + format(eid_key_count))
-    print('eid_count->' + format(eid_count))
+    # print('eid_key_count->' + format(eid_key_count))
+    # print('eid_count->' + format(eid_count))
     key_count = eid_key_count + pid_key_count
     id_count = eid_count + pid_count
     # print('valid_eid_map->' + format(valid_eid_map))
     print('key_count->' + format(key_count))
     print('id_count->' + format(id_count))
+
 
 
 
@@ -329,6 +433,14 @@ if __name__ == '__main__':
     # find_all_kt_track_method("/Users/wuxiaolong/project/qianshou-android/app/src/main/java/com/tantan/x/likecard/favoriteguide/FavoriteGuideAct.kt")
     # find_all_kt_track_method("/Users/wuxiaolong/project/qianshou-android/app/src/main/java/com/tantan/x/like/ui/LikeItemBinder.kt")
     # find_all_java_track_method("/Users/wuxiaolong/project/qianshou-android/app/src/main/java/com/tantan/x/main/recommends/recommend/view/swipe/NewSwipeCardGroup.java")
-    find_all_kt_track_method("/Users/wuxiaolong/project/qianshou-android/app/src/main/java/com/tantan/x/dating/ui/VideoChatActivity.java")
+
+    # find_all_kt_track_method("/Users/wuxiaolong/project/qianshou-android/app/src/main/java/com/tantan/x/dating/ui/VideoChatActivity.java")
+    # find_pageid_method("/Users/wuxiaolong/project/qianshou-android/app/src/main/java/com/tantan/x/dating/ui/VideoChatActivity.java")
+    # find_pageid_method("/Users/wuxiaolong/project/qianshou-android/app/src/main/java/com/tantan/x/vip/MVipBuyAct.kt")
+    # find_pageid_method("/Users/wuxiaolong/project/qianshou-android/app/src/main/java/com/tantan/x/vip/MVipBuyAct.kt")
+
+    # print('valid_pid_map->' + format(valid_pid_map))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# TODO: 再查找pageId()方法
